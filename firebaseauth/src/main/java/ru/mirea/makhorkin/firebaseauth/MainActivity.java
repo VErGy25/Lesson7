11f65b95aad6c217fa05d9685b552cc1bbaffc5b
VErGy25~ <mahorkin2513@gmail.com>
2023-05-28 22:31:37 +0300

    les

--- firebaseauth/src/main/java/ru/mirea/makhorkin/firebaseauth/MainActivity.java
+++ firebaseauth/src/main/java/ru/mirea/makhorkin/firebaseauth/MainActivity.java
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity{
                     }
                 });
     }
-    
+
     private boolean validateForm(String email) {
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
